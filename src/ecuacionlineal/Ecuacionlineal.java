f1dbdf4b7477d27407e72b0d4133750017cb6257
JeanM1996 <jeanpaulmosquera1996@hotmail.com>
2016-10-31 16:37:54 -0500

    prueba1

--- src/ecuacionlineal/Ecuacionlineal.java
+++ src/ecuacionlineal/Ecuacionlineal.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -11,6 +11,7 @@ public class Ecuacionlineal {
     
     public static void main(String[] args) {
       Scanner teclado = new Scanner(System.in);
+        //variables
         int a;
         int b;
         int c;
